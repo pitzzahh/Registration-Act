06a7c36156eb78ddce2c4236197a13c6c7ef7b93
pitzzahh <pitzzahh@gmail.com>
2023-10-28 14:24:49 +0800

    :lipstick: done

--- RegistrationAct/FormConfirmation.xaml.cs
+++ RegistrationAct/FormConfirmation.xaml.cs
@@ -2,10 +2,25 @@ using System.Windows;
 
 namespace RegistrationAct;
 
-public partial class FormConfirmation : Window
+public partial class FormConfirmation
 {
     public FormConfirmation()
     {
         InitializeComponent();
+    }
+
+    private void OnSubmit(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void FormConfirmation_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        LblStudentNo.Text = FormRegistration.StudentInformationClass.SetStudNo.ToString();
+        LblName.Text = FormRegistration.StudentInformationClass.SetFullName;
+        LblProgram.Text = FormRegistration.StudentInformationClass.SetProgram;
+        LblBirthDay.Text = FormRegistration.StudentInformationClass.SetBirthDay;
+        LblGender.Text = FormRegistration.StudentInformationClass.SetGender;
+        LblContactNo.Text = FormRegistration.StudentInformationClass.SetContactNo.ToString();
+        LblAge.Text = FormRegistration.StudentInformationClass.SetAge.ToString();
     }
 }
---
+++ RegistrationAct/FormRegistration.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace RegistrationAct;
+
+/// <summary>
+/// Interaction logic for FormRegistration.xaml
+/// </summary>
+public partial class FormRegistration
+{
+    private string _fullName = null!;
+    private int _age;
+    private long _studentNo;
+    private long _contactNo;
+    private FormConfirmation _formConfirmation = new();
+
+    public FormRegistration()
+    {
+        InitializeComponent();
+    }
+
+    private void FormRegistration_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var listOfPrograms = new[]
+        {
+            "BS Information Technology",
+            "BS Computer Science",
+            "BS Information Systems",
+            "BS in Accountancy",
+            "BS in Hospitality Management",
+            "BS in Tourism Management"
+        };
+
+        foreach (var t in listOfPrograms)
+        {
+            CbPrograms.Items.Add(t);
+        }
+
+        foreach (var gender in new[] { "Male", "Female", "Non-Binary" })
+        {
+            CbGender.Items.Add(gender);
+        }
+    }
+
+    private long StudentNumber(string studNum)
+    {
+        if (Regex.IsMatch(studNum, @"^[0-9]{10,11}$"))
+        {
+            _studentNo = long.Parse(studNum);
+        }
+        else
+        {
+            throw new FormatException("Student Number should be a number! and must be 10 or 11 digits!");
+        }
+        return _studentNo;
+    }
+
+    private long ContactNo(string contact)
+    {
+        if (Regex.IsMatch(contact, @"^[0-9]{10,11}$"))
+        {
+            _contactNo = long.Parse(contact);
+        }
+        else
+        {
+            throw new FormatException("Contact Number should be a number! and must be 10 or 11 digits!");
+        }
+        return _contactNo;
+    }
+
+    private string FullName(string lastName, string firstName, string middleInitial)
+    {
+        if (Regex.IsMatch(lastName, @"^[a-zA-Z]+$") || Regex.IsMatch(firstName, @"^[a-zA-Z]+$") ||
+            Regex.IsMatch(middleInitial, @"^[a-zA-Z]+$"))
+        {
+            _fullName = middleInitial != string.Empty ? lastName + ", " + firstName + ", " + middleInitial : lastName + ", " + firstName;
+        }
+        else
+        {
+            throw new FormatException("Name should be a string!");
+        }
+        return _fullName;
+    }
+
+    private int Age(string age)
+    {
+        if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+        {
+            _age = int.Parse(age);
+        }
+        else if (age == string.Empty)
+        {
+            throw new ArgumentNullException(nameof(age), "Age should not be empty!");
+        }
+        else
+        {
+            throw new FormatException("Age should be a number!");
+        }
+
+        return _age;
+    }
+
+    public static class StudentInformationClass
+    {
+        public static long SetStudNo { get; set; }
+        public static long SetContactNo { get; set; }
+        public static long SetAge { get; set; }
+        public static string SetProgram { get; set; } = string.Empty;
+        public static string SetGender { get; set; } = string.Empty;
+        public static string SetBirthDay { get; set; } = string.Empty;
+        public static string SetFullName { get; set; } = string.Empty;
+    }
+
+    private void OnRegister(object sender, RoutedEventArgs e)
+    {
+        if (!_formConfirmation.IsVisible)
+        {
+            _formConfirmation = new FormConfirmation();
+        }
+
+        try
+        {
+            StudentInformationClass.SetStudNo = StudentNumber(TxtStudentNo.Text);
+            StudentInformationClass.SetProgram = CbPrograms.Text;
+            StudentInformationClass.SetFullName = FullName(TxtLastName.Text, TxtFirstName.Text, TxtMiddleInitial.Text);
+            StudentInformationClass.SetAge = Age(TxtAge.Text);
+            StudentInformationClass.SetGender = CbGender.Text;
+            if (DatePickerBirthday.SelectedDate == null)
+            {
+                throw new ArgumentNullException(nameof(DatePickerBirthday.SelectedDate), "Birthday should not be empty!");
+            }
+            StudentInformationClass.SetBirthDay = DatePickerBirthday.SelectedDate.Value.ToString("yyyy-M-d dd");
+            StudentInformationClass.SetContactNo = ContactNo(TxtContactNo.Text);
+            _formConfirmation.ShowDialog();
+        }
+        catch (FormatException formatException)
+        {
+            MessageBox.Show(formatException.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        catch (ArgumentNullException argumentNullException)
+        {
+            MessageBox.Show(argumentNullException.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        catch (OverflowException overflowException)
+        {
+            MessageBox.Show(overflowException.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        catch (IndexOutOfRangeException outOfRangeException)
+        {
+            MessageBox.Show(outOfRangeException.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void FormRegistration_OnClosing(object? sender, CancelEventArgs e)
+    {
+        Environment.Exit(0);
+        Application.Current.Shutdown(0);
+    }
+}
